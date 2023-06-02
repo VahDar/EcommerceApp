428b6aa61e6703147b45cf7352eed83ee2537de4
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-06-02 19:03:20 +0300

    set up main coordinator

--- EcommerceApp/Application/AppCoordinator.swift
+++ EcommerceApp/Application/AppCoordinator.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator]
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        <#code#>
+    }
+    
+    
+}
--- EcommerceApp/Supports/Coordinator.swift
+++ EcommerceApp/Supports/Coordinator.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+    
+    var parentCoordinator: Coordinator? {get set}
+    var children: [Coordinator] {get set}
+    var navigationController: UINavigationController {get set}
+    
+    func start()
+}
+
+extension Coordinator {
+    func popController() {
+        navigationController.popViewController(animated: true)
+    }
+}
