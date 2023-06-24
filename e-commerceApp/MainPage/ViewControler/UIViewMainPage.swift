8635322b8f469075c870764d1bc492301bfbabb5
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-06-24 21:23:08 +0300

    setUp main page UI and configuration

--- e-commerceApp/MainPage/ViewControler/UIViewMainPage.swift
+++ e-commerceApp/MainPage/ViewControler/UIViewMainPage.swift
@@ -37,11 +37,23 @@ class UIViewMainPage {
     func setUpView() {
         mainVC?.view.addSubview(contentView)
         contentView.addSubview(collectionView)
+        contentView.addSubview(addButton)
         
         //set up contentView
         contentView.topAnchor.constraint(equalTo: (mainVC?.view.topAnchor)!).isActive = true
         contentView.bottomAnchor.constraint(equalTo: (mainVC?.view.bottomAnchor)!).isActive = true
         contentView.leftAnchor.constraint(equalTo: (mainVC?.view.leftAnchor)!).isActive = true
         contentView.rightAnchor.constraint(equalTo: (mainVC?.view.rightAnchor)!).isActive = true
+        
+        //setup collectionview
+        collectionView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        
+        //setup button
+        addButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 25)
+        addButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -30)
+        
     }
 }
