4f0bc22094dd5cbfddd912756f836ef8c88bb7bd
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-24 19:50:15 +0200

    create project

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Vakhtang on 24.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
