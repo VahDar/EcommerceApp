f186119dd9a0c1989f9d09be93479554c3539a54
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-26 20:53:49 +0200

    configured firebase

--- EcommerceApp/EcommerceAppApp.swift
+++ EcommerceApp/EcommerceAppApp.swift
@@ -11,13 +11,23 @@ import Firebase
 @main
 struct EcommerceAppApp: App {
     
-    init() {
-        FirebaseApp.configure()
-        print("Configured Firebase!")
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+        
+    }
+    
 }
