29ec74659e69d9c3b900be0b54473633c519b479
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-30 21:12:11 +0200

    add layout for register screen

---
+++ EcommerceApp/View/Authentication/RegistrationView.swift
@@ -0,0 +1,80 @@
+//
+//  RegistrationView.swift
+//  EcommerceApp
+//
+//  Created by Vakhtang on 30.03.2024.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image("example")
+                    .resizable()
+                    .frame(width: 200, height: 160)
+                Spacer()
+            }
+            
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Adress",
+                          placeholder: "Enter your email")
+                    .textInputAutocapitalization(.never)
+                
+                InputView(text: $fullName,
+                          title: "Full Name",
+                          placeholder: "Enter your full name")
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                    .textInputAutocapitalization(.never)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm your password",
+                          isSecureField: true)
+                    .textInputAutocapitalization(.never)
+            }
+            .padding(.horizontal)
+            .padding(.top, 10)
+            
+            Button(action: {
+                print("sign up")
+            }, label: {
+                Text("Sign Up With Email")
+                    .fontWeight(.semibold)
+            })
+            .foregroundStyle(Color.white)
+            .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            .background(Color.blue)
+            .clipShape(.rect(cornerRadius: 10))
+            .padding(.top, 8)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                Text("Already have an account? ")
+                    + Text("Sign in")
+                        .fontWeight(.bold)
+            }
+            .font(.system(size: 16))
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
