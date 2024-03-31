4405a570dcae1598c79f998ce118327163816a75
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-03-31 15:50:21 +0300

    add reuseble component and finish layout setting screen

--- EcommerceApp/View/SettingView/SettingsScreenView.swift
+++ EcommerceApp/View/SettingView/SettingsScreenView.swift
@@ -13,15 +13,56 @@ struct SettingsScreenView: View {
     var body: some View {
         List {
             Section {
-                
+                HStack {
+                    Text("VD")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .frame(width: 72, height: 72)
+                        .foregroundStyle(Color.white)
+                        .background(Color(.systemGray3))
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading) {
+                        Text("Vah Dar")
+                            .fontWeight(.semibold)
+                            .padding(.top, 4)
+                        Text("test@gmail.com")
+                            .font(.footnote)
+                            .tint(.secondary)
+                    }
+                }
             }
             
             Section("General") {
-                
+                HStack {
+                    SettingRowView(imageName: "gear",
+                                   title: "Version",
+                                   tintColor: Color(.systemGray))
+                    
+                    Spacer()
+                    
+                    Text("1.0.0")
+                        .font(.footnote)
+                        .foregroundStyle(Color(.systemGray))
+                }
             }
             
             Section("Account") {
+                Button {
+                    print("Sign out")
+                } label: {
+                    SettingRowView(imageName: "arrow.left.circle.fill",
+                                   title: "Sign Out",
+                                   tintColor: .red)
+                }
                 
+                Button {
+                    print("account deleted")
+                } label: {
+                    SettingRowView(imageName: "xmark.circle.fill",
+                                   title: "Delete Account",
+                                   tintColor: .red)
+                }
             }
         }
     }
